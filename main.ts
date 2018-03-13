15aa3d0b36a7db59db2bb6bbfa7f5904ff37d1d8
TRICONHOME\himanisingh <himanisingh@triconinfotech.com>
2018-03-13 15:20:34 +0530

    manual updates from menu item and seperate file for updater

--- main.ts
+++ main.ts
@@ -3,97 +3,11 @@ import * as path from 'path';
 import * as url from 'url';
 import electronSquirrel from 'electron-squirrel-startup';
 import * as PDFWindow from 'electron-pdf-window';
-const log = require('electron-log');
-const { autoUpdater } = require('electron-updater');
-
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+import * as updater from './updater';
 
 let win, serve, pdfWin;
 const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
-
-let updater;
-autoUpdater.autoDownload = false;
-
-autoUpdater.on('error', (error) => {
-  dialog.showErrorBox('Error: ', error == null ? 'unknown' : (error.stack || error).toString());
-});
-
-autoUpdater.on('update-available', () => {
-  dialog.showMessageBox({
-    type: 'info',
-    title: 'Found Updates',
-    message: 'Found updates, do you want update now?',
-    buttons: ['Sure', 'No']
-  }, (buttonIndex) => {
-    if (buttonIndex === 0) {
-      autoUpdater.downloadUpdate();
-    } else {
-      updater.enabled = true;
-      updater = null;
-    }
-  });
-});
-
-autoUpdater.on('update-not-available', () => {
-  dialog.showMessageBox({
-    title: 'No Updates',
-    message: 'Current version is up-to-date.'
-  });
-  updater.enabled = true;
-  updater = null;
-});
-
-autoUpdater.on('update-downloaded', () => {
-  dialog.showMessageBox({
-    title: 'Install Updates',
-    message: 'Updates downloaded, application will be quit for update...'
-  }, () => {
-    setImmediate(() => autoUpdater.quitAndInstall());
-  });
-});
-
-// export this to MenuItem click callback
-function checkForUpdates (menuItem, focusedWindow, event) {
-  updater = menuItem;
-  updater.enabled = false;
-  autoUpdater.checkForUpdates();
-}
-
-// autoUpdater.autoDownload = false;
-// autoUpdater.on('checking-for-update', () => {
-//   sendStatusToWindow('Checking for update...');
-// });
-
-// autoUpdater.on('update-available', (info) => {
-//   sendStatusToWindow('Update available.');
-// });
-
-// autoUpdater.on('update-not-available', (info) => {
-//   sendStatusToWindow('Update not available.');
-// });
-
-// autoUpdater.on('error', (err) => {
-//   sendStatusToWindow('Error in auto-updater. ' + err);
-// });
-
-// autoUpdater.on('download-progress', (progressObj) => {
-//   let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
-//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-//   log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
-//   sendStatusToWindow(log_message);
-// });
-
-// autoUpdater.on('update-downloaded', (info) => {
-//   sendStatusToWindow('Update downloaded');
-// });
-
-// function sendStatusToWindow(text) {
-//   log.info(text);
-//   win.webContents.send('message', text);
-// }
 
 const template = [
   {
@@ -141,11 +55,16 @@ const template = [
             focusedWindow.webContents.send('download-pdf');
           }
         }
-      },
+      }
+    ]
+  },
+  {
+    label: 'Updates',
+    submenu: [
       {
-        label: 'Updates',
-        click () {
-         autoUpdater.checkForUpdates();
+        label: 'check for Updates',
+        click (item, focusedWindow) {
+          updater.checkForUpdates(item, focusedWindow);
         }
       }
     ]
@@ -222,7 +141,6 @@ try {
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   app.on('ready', () => {
-  //  autoUpdater.checkForUpdates();
     // autoUpdater.checkForUpdates();
     createWindow();
   });
